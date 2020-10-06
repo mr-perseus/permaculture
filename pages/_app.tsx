0407759377aefe45f9e6e99c2c4536f8cae69022
Jan <janfrei01@gmail.com>
2020-10-06 12:08:17 +0200

    app as component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import { Provider } from '@shopify/app-bridge-react';
@@ -8,6 +7,7 @@ import translations from '@shopify/polaris/locales/en.json';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import React from 'react';
+import { AppPropsType } from 'next/dist/next-server/lib/utils';
 import ClientRouter from '../components/ClientRouter';
 
 const client = new ApolloClient({
@@ -16,32 +16,33 @@ const client = new ApolloClient({
     },
 });
 
-class MyApp extends App {
-    render() {
-        const { Component, pageProps } = this.props;
-        const config = {
-            apiKey: API_KEY,
-            shopOrigin: Cookies.get('shopOrigin') || '',
-            forceRedirect: true,
-        };
+const MyApp: React.FC<AppPropsType> = ({
+    Component,
+    pageProps,
+}: AppPropsType) => {
+    const config = {
+        apiKey: API_KEY,
+        shopOrigin: Cookies.get('shopOrigin') || '',
+        forceRedirect: true,
+    };
 
-        return (
-            <>
-                <Head>
-                    <title>Sample App</title>
-                    <meta charSet="utf-8" />
-                </Head>
-                <Provider config={config}>
-                    <ClientRouter />
-                    <AppProvider i18n={translations}>
-                        <ApolloProvider client={client}>
-                            <Component {...pageProps} />
-                        </ApolloProvider>
-                    </AppProvider>
-                </Provider>
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <Head>
+                <title>Sample App</title>
+                <meta charSet="utf-8" />
+            </Head>
+            <Provider config={config}>
+                <ClientRouter />
+                <AppProvider i18n={translations}>
+                    <ApolloProvider client={client}>
+                        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                        <Component {...pageProps} />
+                    </ApolloProvider>
+                </AppProvider>
+            </Provider>
+        </>
+    );
+};
 
 export default MyApp;
