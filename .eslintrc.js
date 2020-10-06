e0b248d2b29a781d8ba1f523fedf9676ee721117
Jan <janfrei01@gmail.com>
2020-10-06 17:21:36 +0200

    Renames. Linter updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,24 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'plugin:security/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         project: './tsconfig.json',
     },
-    plugins: ['@typescript-eslint', 'prettier'],
+    plugins: ['@typescript-eslint', 'prettier', 'security', 'no-secrets'],
     rules: {
         'prettier/prettier': ['warn'],
         'react/jsx-indent': ['warn', 4],
         'react/jsx-indent-props': ['warn', 4],
+        'react/jsx-one-expression-per-line': 'off',
+
+        // See https://github.com/nickdeis/eslint-plugin-no-secrets
+        'no-secrets/no-secrets': 'error',
 
         // TODO re-enable this rule
         'no-console': 'off',
-
-        'react/jsx-one-expression-per-line': 'off',
     },
 };
